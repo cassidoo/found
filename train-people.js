80421d01a36668d5fe29b26987d3ea2c82d69944
cassidoo <cassidy@clarifai.com>
2015-11-11 16:33:34 -0500

    Add in email notification on success

--- train-people.js
+++ train-people.js
@@ -84,6 +84,28 @@ function predict(imgurl) {
           text: 'Thank you so much for your efforts.',
           imageUrl: obj.url
         });
+        $.ajax({
+        type: "POST",
+        url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+        data: {
+          'key': 'xqVOxJi9EcJE0yTYRrtPw',
+          'message': {
+            'from_email': 'ijoosong@gmail.com',
+            'to': [
+                {
+                  'email': 'cassidoo@gmail.com',
+                  'name': 'Cassidy Williams',
+                  'type': 'to'
+                },
+              ],
+            'autotext': 'true',
+            'subject': 'Your Loved One Found!',
+            'html': 'Hi Cassidy Williams!  We have exciting news for you.  It seems that we located your loved one, '+nombre+'!'
+          }
+        }
+       }).done(function(response) {
+         console.log(response); // if you're into that sorta thing
+        });
       }
     },
     promiseRejected
